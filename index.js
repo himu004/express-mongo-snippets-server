6fb9098b4771e5353116edb187f038cc3165a2b9
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-06-03 16:41:40 +0600

    Put/update API implemented

--- index.js
+++ index.js
@@ -29,34 +29,51 @@ async function run() {
     // Send a ping to confirm a successful connection
     //! APIs for send data on MongoDB server / database
 
-    const database = client.db('usersDB');
-    const usersCollection = database.collection('users');
+    const database = client.db("usersDB");
+    const usersCollection = database.collection("users");
 
-    app.get('/crudusers', async (req, res) => {
-      const cursor = usersCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-
-    app.post('/crudusers', async(req, res) => {
-      const user = req.body;
-      console.log('new user: ', user);
-      const result = await usersCollection.insertOne(user);
-      res.send(result)
-
-
-    })
-
-    app.delete('/crudusers/:id', async(req, res) => {
-      
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result =  await usersCollection.deleteOne(query);
+    app.get("/crudusers", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
+    });
 
-    
-    }) 
+    app.get("/crudusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await usersCollection.findOne(query);
+      res.send(user);
 
+    })
+
+    app.post("/crudusers", async (req, res) => {
+      const user = req.body;
+      console.log("new user: ", user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/crudusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true}
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
+
+    app.delete("/crudusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -69,7 +86,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-//****************************************** MONGODB ENDS ******************************************************\\ 
+//****************************************** MONGODB ENDS ******************************************************\\
 //! Middleware
 app.use(cors());
 app.use(express.json());
